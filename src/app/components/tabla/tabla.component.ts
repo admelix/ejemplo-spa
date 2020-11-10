aa94b96176241f7ce0871cc4b5d3fdb9523c9ad7
Jose Francisco sakuda castro <jsakuda@Joses-MacBook-Pro.local>
2020-11-09 19:35:41 -0500

    clase lunes 9-11

--- src/app/components/tabla/tabla.component.ts
+++ src/app/components/tabla/tabla.component.ts
@@ -7,6 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TablaComponent implements OnInit {
 
+  nombre: string = 'Jose Sakuda';
+  nombre2: string = 'JoSE SAKudA';
+
+  arreglo: number[] = [1,2,3,4,5,6,7,8,9,10];
+  PI:number = Math.PI;
+  porcentaje: number = 0.234;
+  salario:number = 12345.2;
+  fecha: Date = new Date();
+  idioma: string = '';
+  videoUrl: string = 'https://www.youtube.com/watch?v=7f0CfWNc5tQ';
+  pass: string = 'HOLAAAAAAAAAAAAAAAA'
+  activar: boolean = true;
+
+  objetoPersona = {
+    nombre: 'Jose',
+    apellido: 'Sakuda',
+    edad: 34,
+    direccion:{
+      calle: 'Avenida siempre viva',
+      casa: 4
+    }
+  };
+
   constructor() { }
 
   ngOnInit(): void {
